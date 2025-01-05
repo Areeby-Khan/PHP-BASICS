ecc908119aefdcc321288517cd456ffa6a23a60e
Areeby-Khan <areebalouch@gmail.com>
2025-01-05 21:09:39 +0500

    Add files via upload

---
+++ php_basics/Variables.php
@@ -0,0 +1,40 @@
+<?php
+
+//Variables : These are like Storage location where you can store any name , age, value , any data .
+//These are like Abstracted storage location .
+
+echo "<h1 style='text-align:center; color:green;font-size:3rem;font-family:arial;'>Variables</h1>";
+$variable="This is a Variable.";
+echo $variable;
+echo "<br>";
+echo $variable;
+echo "<br>";
+
+$num=200;
+echo "<br>";
+echo $num+2;
+echo "<br>";
+echo $num+41;
+echo "<br>";
+echo $num+20;
+echo "<br>";
+echo var_dump($num);
+echo "<br>";
+$Num=20;
+echo "<br>";
+echo $Num;
+
+
+//Variable are case sensitive : $num and $Num are two different variables .
+echo "<br>";
+$_Numbers=10;
+echo $_Numbers;
+echo "<br>";
+$a=$b=30;
+echo $a;
+echo "<br>";
+echo $b;
+
+
+
+?>
