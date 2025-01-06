c2386c676e17d5f2ddc697e2acd4f0fd94b200a1
Areeby-Khan <areebalouch@gmail.com>
2025-01-06 18:00:38 +0500

    Add files via upload

---
+++ php_basics/display-error.php
@@ -0,0 +1,11 @@
+<?php
+
+//To display error in the screen .
+
+echo phpinfo();
+//Here you in the php.ini file 
+// then search for ( display_error ) and after that display_error = on 
+//this will help you to show what kind of error you are facing
+
+
+?>
